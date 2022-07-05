311ed36e6a14a8f5d9388011377282a34a0da1dd
Xavier <xherriot@protonmail.com>
2022-07-05 15:30:48 +0200

    refactor(*): general enhancement

--- src/Connector.d.ts
+++ src/Connector.d.ts
@@ -1,23 +1,11 @@
-/**
- * @public
- */
 export type MongoDBLink = string
 
-/**
- * @public
- */
 export type MongoDBName = string
 
-/**
- * @public
- */
 export type MongoDBParams = {
   [key: string]: (string | number | boolean)
 }
 
-/**
- * @public
- */
 export type MongoDBOptions = {
   [key: string]: (string | number | boolean)
 }
