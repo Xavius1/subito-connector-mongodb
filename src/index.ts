cd6955a880342f7e5b9caa60dc9e75e1c22603ce
Xavier <xherriot@protonmail.com>
2022-07-06 08:40:47 +0200

    fix(eslint): import ts config

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  *
  * @packageDocumentation
  */
-export { default as MongoDBConnector } from './Connector.js';
-export { default as MongoDBHelper } from './Helper.js';
-export { default as MongoDBRepository } from './Repository.js';
-export { default as MongoDBPaginator } from './Paginator.js';
+export { default as MongoDBConnector } from './Connector';
+export { default as MongoDBHelper } from './Helper';
+export { default as MongoDBRepository } from './Repository';
+export { default as MongoDBPaginator } from './Paginator';
